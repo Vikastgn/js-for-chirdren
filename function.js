45fa4d6271212477206291ec43659ee3a55c5ccc
Vikastgn <vikamarygina@yandex.ru>
2024-02-22 18:14:48 +0300

    js

---
+++ function.js
@@ -0,0 +1,10 @@
+function add (arg1, arg2) {
+    return arg1 + arg2;
+}
+
+function multiply (arg1, arg2) {
+    return arg1 * arg2;
+}
+
+let res = multiply (36325, 9824);
+console.log (add(res,777));
